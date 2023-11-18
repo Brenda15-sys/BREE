6f4af9fd5d654ed41ca6c12cbcdf121f557b03e8
Brenda Kimura <khamalibreda@gmail.com>
2023-11-18 23:07:18 +0300

    project continues

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './Home.jsx';
 import About from './About.jsx';
 import Blog from './Blog.jsx';
 import Contact from './Contact.jsx';
+import Project from './Project.jsx';
 import './App.css'
 
 function App() {
@@ -15,14 +16,17 @@ function App() {
          <nav>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
+            <NavLink to="/project">Project</NavLink>
             <NavLink to="/blog">Blog</NavLink>
             <NavLink to="/contact">Contact</NavLink>
+            
           </nav>
         </header>
         <main>
         <Routes>
           <Route path="/"element={<Home />} />
           <Route path="about" element={<About />} />
+          <Route path="project" element={<Project />} />
           <Route path="blog" element={<Blog />} />
           <Route path="contact" element={<Contact />} />
          </Routes>
---
+++ src/Project.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import TodoCard from "./ToDoCard";
+import "./Project.css";
+
+
+            
+
+
+function Project() {
+  const [items, setItems] = useState([]);
+  const [newItemText, setNewItemText] = useState("");
+
+  const addItem = () => {
+    if (newItemText.trim() !== "") {
+      setItems([...items, { text: newItemText, isCompleted: false }]);
+      setNewItemText("");
+    }
+  };
+
+  const toggleItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems[index].isCompleted = !updatedItems[index].isCompleted;
+    setItems(updatedItems);
+  };
+
+  const clearList = () => {
+    setItems([]);
+  };
+
+  return (
+    <div className="project-wrapper">
+        <ToDoCard>
+    <div className="app-container background-image">
+      <h1> My To-dos List</h1>
+ 
+      <h3><i>
+        &#x1F50D; To-Do List
+        </i>
+      </h3>
+      <div>
+        <input
+          type="text"
+          value={newItemText}
+          onChange={(e) => setNewItemText(e.target.value)}
+          placeholder="&#x1F50D; search to-dos...."
+        />
+        <button onClick={addItem}>Add Item</button>
+        <button onClick={clearList}>Clear List</button>
+      </div>
+      <ul>
+        {items.map((item, index) => (
+          <li
+            key={index}
+            onClick={() => toggleItem(index)}
+            style={{
+              textDecoration: item.isCompleted ? "line-through" : "none",
+              cursor: "pointer",
+            }}
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+      
+    
+    </div>
+     </ToDoCard> 
+    </div>
+  );
+  
+}
+
+export default Project;
+
+ 
+        
